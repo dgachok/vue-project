2a27a8bf290a0f9c4405dea709cf3cd92426233a
Dima <d.gachok@gmail.com>
2018-04-15 22:05:16 +0300

    add bootstrap and change structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="hello">
+            <h1>Welcome to TodoList App!</h1>
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6 d-flex align-items-center my-3">
+                    <input class="form-control" type="text" placeholder="Add item">
+                    <button type="button" class="btn btn-primary mx-3">Add</button>
+                </div>
+                <div class="col-3"></div>
+            </div>
+            <List :list="list"></List>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import List from './components/List.vue'
+    import List from './components/List.vue'
+    import 'bootstrap/dist/css/bootstrap.css'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-    List
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            List
+        },
+        data: () => ({
+            list: [
+                {id: 0, text: 'Овощи'},
+                {id: 1, text: 'Сыр'},
+                {id: 2, text: 'Что там ещё люди едят?'}
+            ]
+        })
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    .btn-add {
+        padding: 0 10px;
+        cursor: pointer;
+        color: blue;
+    }
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,20 +1,29 @@
 <template>
-  <div>
-    <li v-for="item in list"
-        v-bind:key="item.id">1{{item.text}}</li>
-   </div>
+    <table class="table">
+        <tbody>
+        <tr v-for="item in list"
+            v-bind:key="item.id">
+            <th scope="row"
+                width="80%"
+                class="align-middle">
+                {{item.text}}
+            </th>
+            <td width="20%">
+                <button type="button" class="btn btn-success mx-2">Done</button>
+                <button type="button" class="btn btn-danger mx-2">Remove item</button>
+            </td>
+        </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
-export default {
-  name: 'List',
-  props: {
-    list: Array
-  },
-   created: function () {
-      console.log('value list: ' + this.list)
+    export default {
+        name: 'List',
+        props: {
+            list: Array
+        }
     }
-}
 </script>
 
 <style scoped>
